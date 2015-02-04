b821ebc42d482ad456183595123e4ab70d5799ab
kumarph <p.s.sathishkumar@gmail.com>
2015-02-04 08:35:14 +0530

    first cut

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "<center><b>Welcome to Cool Cabs </b><br>";
+        echo "Your One stop point for travel requirements.</center>"
+        
+        ?>
+    </body>
+</html>
